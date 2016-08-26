3d8a1217a8872521165cf8185474e8bc6afbf27c
alexandre <alebencz@gmail.com>
2016-08-26 12:04:05 -0300

    Put the correct 'hello.c'

--- netil/tests/hello.c
+++ netil/tests/hello.c
@@ -1,17 +1,5 @@
-#if __ORANGEC__
-#include <stdio.h>
-#endif
-
-#if __ORANGEC__
-int main()
-#else
 void main()
-#endif
 {
   printf("Hello World! :)\n");
-#if __ORANGEC__
-  return 0;
-#else
   exit(0);
-#endif
 }
